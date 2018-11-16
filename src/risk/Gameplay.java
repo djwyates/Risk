f0c2393b5e603e5ca64585b25bb101d0e8e0e19f
David Yates <djwyates@users.noreply.github.com>
2018-11-16 13:03:53 -0800

    added random assignment of countries method

--- src/risk/Gameplay.java
+++ src/risk/Gameplay.java
@@ -3,4 +3,11 @@ package risk;
 
 public class Gameplay {
     
+    Player owner;
+    
+    static public void assignCountries() {
+        for (Country country : RiskMap.getCountryList()) {
+            
+        }
+    }
 }
