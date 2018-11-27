213922396a98eef780185c799b0a283670a58ab6
PandaPine <44711970+PandaPine@users.noreply.github.com>
2018-11-26 19:27:11 -0800

    Added Insruction

--- src/risk/Titlescreen.java
+++ src/risk/Titlescreen.java
@@ -58,12 +58,12 @@ public class Titlescreen {
         g.drawImage(SETUP_IMAGE,0,0,Window.MENU_WINDOW_WIDTH,Window.MENU_WINDOW_HEIGHT,frame);
         Button.setupHandler(x, y);
     }
-    //////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     static public void instructionsHand(int x, int y,Graphics2D g)throws FileNotFoundException, FontFormatException, IOException{
         if(instructionsActive)
         instructionsHandler(x,y,g);
     }
-    //////////////////////////////////////////////////////////////////////////
+
     static private void instructionsHandler(int x, int y,Graphics2D g)throws FileNotFoundException, FontFormatException, IOException {
 //        if(Connect.gameStarted()==false) {     
 //        }
@@ -71,18 +71,62 @@ public class Titlescreen {
 //        }
         
            // if(!InstuctionsConnected){
-            g.setColor(Color.cyan);
+            g.setColor(Color.CYAN);
             g.fillRect(0, 0, Window.MENU_WINDOW_WIDTH, Window.MAP_WINDOW_HEIGHT);
            // InstuctionsConnected = false;
       //  }
+      
+    //Title (Risk Rules)
             g.setColor(Color.black);
             g.setFont(new Font("Arial",Font.PLAIN,50));
-            g.drawString("Risk Rules",25,500);         
+            g.drawString("Risk Rules",300,90);         
+    //1st Sentence (Spilts up Countries)
+            g.drawString("1. Countries are randomly divided between the amount of players.",50,130);         
+    //2nd Sentence ()
+            g.drawString("2.",50,160);         
+    //3rd Sentence ()
+            g.drawString("3.",50,190);         
+    //4th Sentence ()
+            g.drawString("4.",50,220);         
+    //5th Sentence ()
+            g.drawString("5.",50,250);         
+    //6th Sentence ()
+            g.drawString("6.",50,280);         
+    //7th Sentence ()
+            g.drawString("7.",50,310);         
+    //8th Sentence ()
+            g.drawString("8.",50,340);         
+    //9th Sentence ()
+            g.drawString("9.",50,370);         
+    //10th Sentence ()
+            g.drawString("10.",50,400);         
+    //11th Sentence ()
+            g.drawString("11.",50,430);         
+    //12th Sentence ()
+            g.drawString("12.",50,460);         
+    //13th Sentence ()
+            g.drawString("13.",50,490);         
+    //14th Sentence ()
+            g.drawString("14.",50,520);         
+    //15th Sentence ()
+            g.drawString("15.",50,550);         
+    //16th Sentence ()
+            g.drawString("16.",50,580);         
+    //17th Sentence ()
+            g.drawString("17.",50,610);         
+    //18th Sentence ()
+            g.drawString("18.",50,640);         
+    //19th Sentence ()
+            g.drawString("19.",50,670);         
+    //20th Sentence ()
+            g.drawString("20.",50,700);         
+    //21th Sentence ()
+            g.drawString("21.",50,730);         
             
     }
         
     
-    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     static public void startGame(Risk frame) {
         game = new Gameplay(frame);
         mainActive = false;
